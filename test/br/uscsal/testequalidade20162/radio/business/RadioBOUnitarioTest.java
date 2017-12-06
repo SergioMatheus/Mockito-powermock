ae0e6a12c1cb002a05a2ad6f9c0862bac26642bb
Sérgio Matheus <matheus_imagina@hotmail.com>
2017-12-05 22:00:54 -0300

    Inicio dos Testes Unitarios

--- test/br/uscsal/testequalidade20162/radio/business/RadioBOUnitarioTest.java
+++ test/br/uscsal/testequalidade20162/radio/business/RadioBOUnitarioTest.java
@@ -1,12 +1,27 @@
 package br.uscsal.testequalidade20162.radio.business;
 
+import org.junit.Test;
+
+import br.uscsal.testequalidade20162.radio.exceptions.RegistroDuplicadoException;
+import br.uscsal.testequalidade20162.radio.exceptions.RegistroNaoEncontradoException;
+
 public class RadioBOUnitarioTest {
 	
-	// Método a ser testado: public static void adicionarMusica(String tituloAlbum, String nomeMusica) throws
+	// Metodo a ser testado: public static void adicionarMusica(String tituloAlbum, String nomeMusica) throws
 	// RegistroNaoEncontradoException,
-	// Verificar se a inclusão de uma música duplicada em um album retorna a exceção adequada.
-	// LEMBRE-SE DE QUE ESTE TESTE DEVE SER UNITÁRIO!!!
-	public void adicionarAtorNascidoAntesLancamentoFilme() {
-
+	// Verificar se a inclusÃ£o de uma musica duplicada em um album retorna a exceÃ§Ã£o adequada.
+	// LEMBRE-SE DE QUE ESTE TESTE DEVE SER UNITÃ�RIO!!!
+	
+	@Test
+	public void testeUnitarioAdicionarMusica() throws RegistroNaoEncontradoException, RegistroDuplicadoException {
+		
+		String tituloAlbum="Odiosa Natureza Humana";
+		String nomeMusica="De saco cheio e de mal humor";
+		RadioBO.adicionarMusica(tituloAlbum, nomeMusica);
+		
+		
+		
+		
+		
 	}
 }
