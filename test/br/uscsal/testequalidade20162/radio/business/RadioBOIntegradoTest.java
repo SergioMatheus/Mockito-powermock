f4b8d99692703e0fd8863491949188b81886a54e
Sérgio Matheus <matheus_imagina@hotmail.com>
2017-12-06 21:10:35 -0300

    Teste Integrado com bug porém acredito que esteja funcional

--- test/br/uscsal/testequalidade20162/radio/business/RadioBOIntegradoTest.java
+++ test/br/uscsal/testequalidade20162/radio/business/RadioBOIntegradoTest.java
@@ -1,16 +1,17 @@
 package br.uscsal.testequalidade20162.radio.business;
 
 import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-//import org.powermock.*;
-
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.*;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroDuplicadoException;
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroNaoEncontradoException;
 
-//@RunWith(PowerMockRunner.class)
-//@PrepareForTest(XxxService.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({AlbumDao.class, MusicaDao.class, Musica.class, Album.class})
 public class RadioBOIntegradoTest {
 
 	// Método a ser testado: public static void adicionarMusica(String tituloAlbum, String nomeMusica) throws
@@ -20,13 +21,44 @@ public class RadioBOIntegradoTest {
 	
 	RadioBO radio = Mockito.mock(RadioBO.class);
 	
+	@Mock
+	AlbumDao albumDaoMock;
+	MusicaDao musicaDaoMock;
+	
+	@Before
+	public void setUp() throws ParseException, RegistroNaoEncontradoException, RegistroDuplicadoException {
+		RadioBo radio = new RadioBO();
+		String nomedoAlbum = "JEsús";
+		DateUtil date = null;
+		Album album = new Album(nomeAlbum, date, TipoMidia.CD);
+		
+		String nomeMusica = "Esú";
+		Integer duracao = 3;
+		String nomeCantor = "Baco - Exu do blues";
+		Musica musica = new Musica(nomeMusica, duracao, nomeCantor);
+		
+		PowerMockito.mockStatic(albumDaoMock.getClass());
+		PowerMockito.mockStatic(musicaDaoMock.getClass());
+		
+		PowerMockito.doNothing().when(albumDaoMock).incluir(album);
+		PowerMockito.doNothing().when(musicaDaoMock).incluir(musica);
+		
+		PowerMockito.doNothing().when(albumDaoMock).buscarPorTitulo(nomeAlbum);
+		PowerMockito.doNothing().when(musicaDaoMock).buscarPorNome(nomeMusica);
+		
+		radio.incluirAlbum(nomeAlbum, dataLancamento, TipoMidia.CD);
+		radio.incluirMusica(nomeMusica, duracao, nomeInterprete);
+		
+		radio.adicionarMusica(nomeAlbum, nomeMusica);
+
+	}
+	
 	@Test
 	public void testeIntegradoAdicionarMusica() throws RegistroNaoEncontradoException, RegistroDuplicadoException {
 		String tituloAlbum = "Esú";
 		String nomeMusica = "Esú";
-		Mockito.doNothing().when(radio).adicionarMusicaInstancia(tituloAlbum, nomeMusica);
-		radio.adicionarMusicaInstancia(tituloAlbum, nomeMusica);
-		Mockito.verify(radio).adicionarMusicaInstancia(tituloAlbum, nomeMusica);
+		
+		RadioBo.adicionarMusica(tituloAlbum, nomeMusica);
 		
 	}	
 }
