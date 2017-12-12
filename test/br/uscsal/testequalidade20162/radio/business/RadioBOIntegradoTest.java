fb9e0bb270207e7b2178124bc14115af8804f52d
100904037 <100904037@PT1L209.ucsal.br>
2017-12-12 10:01:11 -0300

    Whitebox - Mockito

--- test/br/uscsal/testequalidade20162/radio/business/RadioBOIntegradoTest.java
+++ test/br/uscsal/testequalidade20162/radio/business/RadioBOIntegradoTest.java
@@ -1,65 +1,54 @@
 package br.uscsal.testequalidade20162.radio.business;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.*;
-import org.powermock.modules.junit4.PowerMockRunner;
+import java.text.ParseException;
+import java.util.Date;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import br.uscsal.testequalidade20162.radio.domain.Album;
+import br.uscsal.testequalidade20162.radio.domain.Musica;
+import br.uscsal.testequalidade20162.radio.enums.TipoMidia;
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroDuplicadoException;
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroNaoEncontradoException;
+import br.uscsal.testequalidade20162.radio.util.DateUtil;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({AlbumDao.class, MusicaDao.class, Musica.class, Album.class})
 public class RadioBOIntegradoTest {
 
-	// Método a ser testado: public static void adicionarMusica(String tituloAlbum, String nomeMusica) throws
+	// Método a ser testado: public static void adicionarMusica(String
+	// tituloAlbum, String nomeMusica) throws
 	// RegistroNaoEncontradoException,
-	// Verificar se a inclusão de uma música duplicada em um album retorna a exceção adequada.
+	// Verificar se a inclusão de uma música duplicada em um album retorna a
+	// exceção adequada.
 	// LEMBRE-SE DE QUE ESTE TESTE DEVE SER INTEGRADO!!!
-	
-	RadioBO radio = Mockito.mock(RadioBO.class);
-	
-	@Mock
-	AlbumDao albumDaoMock;
-	MusicaDao musicaDaoMock;
-	
+
 	@Before
 	public void setUp() throws ParseException, RegistroNaoEncontradoException, RegistroDuplicadoException {
-		RadioBo radio = new RadioBO();
+		RadioBO radio = new RadioBO();
 		String nomedoAlbum = "JEsús";
-		DateUtil date = null;
-		Album album = new Album(nomeAlbum, date, TipoMidia.CD);
-		
+		String dataString = "05/07/2007";
+		DateUtil dateutil = new DateUtil();
+		Date date = dateutil.parse(dataString);
+		Album album = new Album(nomedoAlbum, date, TipoMidia.CD);
+
 		String nomeMusica = "Esú";
 		Integer duracao = 3;
 		String nomeCantor = "Baco - Exu do blues";
 		Musica musica = new Musica(nomeMusica, duracao, nomeCantor);
-		
-		PowerMockito.mockStatic(albumDaoMock.getClass());
-		PowerMockito.mockStatic(musicaDaoMock.getClass());
-		
-		PowerMockito.doNothing().when(albumDaoMock).incluir(album);
-		PowerMockito.doNothing().when(musicaDaoMock).incluir(musica);
-		
-		PowerMockito.doNothing().when(albumDaoMock).buscarPorTitulo(nomeAlbum);
-		PowerMockito.doNothing().when(musicaDaoMock).buscarPorNome(nomeMusica);
-		
-		radio.incluirAlbum(nomeAlbum, dataLancamento, TipoMidia.CD);
-		radio.incluirMusica(nomeMusica, duracao, nomeInterprete);
-		
-		radio.adicionarMusica(nomeAlbum, nomeMusica);
+
+		radio.incluirAlbum(nomedoAlbum, date, TipoMidia.CD);
+		radio.incluirMusica(nomeMusica, duracao, nomeCantor);
+
+		radio.adicionarMusica(nomedoAlbum, nomeMusica);
 
 	}
-	
-	@Test
-	public void testeIntegradoAdicionarMusica() throws RegistroNaoEncontradoException, RegistroDuplicadoException {
-		String tituloAlbum = "Esú";
-		String nomeMusica = "Esú";
-		
-		RadioBo.adicionarMusica(tituloAlbum, nomeMusica);
-		
-	}	
-}
 
+	@Test(expected = RegistroDuplicadoException.class)
+	public void testeIntegradoAdicionarMusica() throws RegistroNaoEncontradoException, RegistroDuplicadoException {
+		String tituloAlbum = "JEsús";
+		String nomeMusica = "Esú";
+
+		RadioBO.adicionarMusica(tituloAlbum, nomeMusica);
+
+	}
+}
--- test/br/uscsal/testequalidade20162/radio/business/RadioBOUnitarioTest.java
+++ test/br/uscsal/testequalidade20162/radio/business/RadioBOUnitarioTest.java
@@ -1,12 +1,26 @@
 package br.uscsal.testequalidade20162.radio.business;
 
 import java.util.Date;
-import org.junit.Test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import br.uscsal.testequalidade20162.radio.domain.Album;
+import br.uscsal.testequalidade20162.radio.domain.Musica;
 import br.uscsal.testequalidade20162.radio.enums.TipoMidia;
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroDuplicadoException;
 import br.uscsal.testequalidade20162.radio.exceptions.RegistroNaoEncontradoException;
+import br.uscsal.testequalidade20162.radio.persistence.AlbumDao;
+import br.uscsal.testequalidade20162.radio.persistence.MusicaDao;
+import br.uscsal.testequalidade20162.radio.util.DateUtil;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ AlbumDao.class, MusicaDao.class })
 public class RadioBOUnitarioTest {
 
 	// Metodo a ser testado: public static void adicionarMusica(String
@@ -14,28 +28,27 @@ public class RadioBOUnitarioTest {
 	// RegistroNaoEncontradoException,
 	// Verificar se a inclusÃ£o de uma musica duplicada em um album retorna a
 	// exceÃ§Ã£o adequada.
-	// LEMBRE-SE DE QUE ESTE TESTE DEVE SER UNITÃ�RIO!!!	
+	// LEMBRE-SE DE QUE ESTE TESTE DEVE SER UNITÃ�RIO!!!
 
-	@Test(expected = RegistroNaoEncontradoException.class)
-	public void testeUnitarioAdicionarMusicaRegistroNaoEncontrado()
-			throws RegistroNaoEncontradoException, RegistroDuplicadoException {
-		String tituloAlbum2 = "Dark Sky Paradise";
-		String nomeMusica2 = "I Know";
-		RadioBO.adicionarMusica(tituloAlbum2, nomeMusica2);
+	@Before
+	public void setup() {
+
+		PowerMockito.mockStatic(AlbumDao.class);
+		PowerMockito.mockStatic(MusicaDao.class);
+		
+		String datas = "05/06/2007";
+		DateUtil dateutil = new DateUtil();
+		Date data = dateutil.parse(datas);
+		
+		Whitebox.setInternalState(AlbumDao.class, "incluirAlbum", "Musica 1",data, TipoMidia.CD );
+		
 	}
 
 	@Test(expected = RegistroDuplicadoException.class)
 	public void testeUnitarioAdicionarMusicaRegistroDuplicado()
 			throws RegistroNaoEncontradoException, RegistroDuplicadoException {
-		String tituloAlbum = "Odiosa Natureza Humana";
-		String nomeMusica = "De saco cheio e de mal humor";
-		Date dataLancamento = null;
-		Integer duracao = 2;
-		String nomeInterpretePrincipal="Matanza";
-		TipoMidia tipoMidia = TipoMidia.DIGITAL;
-		RadioBO.incluirAlbum(tituloAlbum, dataLancamento, tipoMidia);
-		RadioBO.incluirMusica(nomeMusica, duracao, nomeInterpretePrincipal);
-		RadioBO.adicionarMusica(tituloAlbum, nomeMusica);
-		RadioBO.adicionarMusica(tituloAlbum, nomeMusica);
+		
+		
+		
 	}
 }
